8449734d83f519566d845f91b531209c9cd8a068
LiuCong2017 <2018568647@qq.com>
2022-03-04 12:17:20 +0800

    add new

--- cpp_source/1_introduction/cpp_io.cpp
+++ cpp_source/1_introduction/cpp_io.cpp
@@ -2,3 +2,26 @@
 // Created by kavin on 2022/2/8.
 //
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int oneInt1, oneInt2;
+    char strArray[20];
+    string str;
+    double oneDouble;
+    char oneChar = 'a';
+    cout<<"input 2 int, 1 char and 1 float,";
+    cout<<"separate with space, tab or Enter:"<<endl;
+    cin>>oneInt1>>oneInt2>>oneChar>>strArray>>oneDouble;
+    str=strArray;
+    cout<<"The value input is:"<<endl;
+    cout<<"string is: \t\t"<<str<<endl
+        <<"two int is: \t"<<oneInt1<<" and\t"<<oneInt2<<endl
+        <<"char is: \t\t"<<oneChar<<"\n"
+        <<"float is: \t\t"<<oneDouble<<endl;
+
+    return 0;
+}
