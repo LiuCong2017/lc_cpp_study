f2d02bde4da893b2d16b45c7a8c42f5f70d6fd47
LiuCong2017 <2018568647@qq.com>
2022-03-11 02:36:29 +0800

    add new

--- cpp_study/6_week/1_struct.cpp
+++ cpp_study/6_week/1_struct.cpp
@@ -10,11 +10,32 @@ struct Student
     double score;
 };
 
+int AddStudentScore(Student &c, double value)
+{
+    c.score +=  value;
+    return 1;
+}
+
 int main()
 {
     Student a;
     strcpy(a.name,"Eva");
     a.gender = 'M';
     a.score = 100;
+
+    AddStudentScore(a,100);
+
+    Student b[10];
+    b[0].score = 50;
+
+    Student *p = &a;
+    a.score = 50;
+    p->score = 100; //指针方式访问结构体
+
+    p = b; //指向结构体数组，p指向b[0]
+    p->gender = 'M';
+
+    p++; //p指向b[1].03665
+    p->gender = 'F';
 
 }
